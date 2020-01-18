55fb9b27b674a7a996e71dd6bc60fe8d4f7d55e1
Victor Gautschi <victor.gautschi@gmail.com>
2020-01-18 19:35:56 +0200

    3rd set of changes

---
+++ Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Iesi.Collections.Generic;
+
+namespace NHibernateTutorial2
+{
+    public class Order
+    {
+        public virtual Guid Id { get; set; }
+        public virtual DateTime Ordered { get; set; }
+        public virtual DateTime? Shipped { get; set; }
+        public virtual Location ShipTo { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Order Id: {0}", Id);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,137 @@
 ﻿using System;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using HibernatingRhinos.Profiler.Appender.NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Linq;
 
 namespace NHibernateTutorial2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var cfg = ConfigureNHibernate();
+            var sessionFactory = cfg.BuildSessionFactory();
+
+            Guid id;
+            using (var session = sessionFactory.OpenSession())
+
+            // transaction to create customers and orders
+            using (var tx = session.BeginTransaction())
+            {
+                var newCustomer = CreateCustomer();
+                Console.WriteLine("New Customer:");
+                Console.WriteLine(newCustomer);
+                session.Save(newCustomer);
+
+                /* Manually having to save Orders for the customer here 
+                foreach (var order in newCustomer.Orders)
+                {
+                    session.Save(order);
+                }
+                */
+
+                id = newCustomer.Id;
+                tx.Commit();
+            }
+
+            using (var session = sessionFactory.OpenSession())
+
+            // transaction to bring up (load) existing customers and orders
+            using (var tx = session.BeginTransaction())
+            {
+                /* join and fetch happening at once at query level */
+                var query = from customer in session.Query<Customer>()
+                            where customer.Id == id
+                            select customer;
+                var reloaded = query.Fetch(x => x.Orders).ToList().First();
+
+                //var reloaded = session.Load<Customer>(id);
+                Console.WriteLine("Reloaded:");
+                Console.WriteLine(reloaded);
+                
+                /*
+                Console.WriteLine("The orders were ordered by: ");
+
+                foreach (var order in reloaded.Orders)
+                {
+                    Console.WriteLine(order.Customer);
+                }
+                */
+
+                tx.Commit();
+            }
+
+            Console.WriteLine("Press <ENTER> to exit...");
+            Console.ReadLine();
+        }
+
+        private static Customer CreateCustomer()
+        {
+            var customer = new Customer
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Points = 100,
+                HasGoldStatus = true,
+                MemberSince = new DateTime(2012, 1, 1),
+                CreditRating = CustomerCreditRating.Good,
+                AverageRating = 42.42424242,
+                Address = CreateLocation()
+            };
+
+            var order1 = new Order
+            {
+                Ordered = DateTime.Now
+            };
+
+            customer.AddOrder(order1);
+
+            var order2 = new Order
+            {
+                Ordered = DateTime.Now.AddDays(-1),
+                Shipped = DateTime.Now,
+                ShipTo = CreateLocation()
+            };
+
+            customer.AddOrder(order2);
+            return customer;
+        }
+
+        private static Location CreateLocation()
+        {
+            return new Location
+            {
+                Street = "123 Somewhere Avenue",
+                City = "Nowhere",
+                Province = "Alberta",
+                Country = "Canada"
+            };
+        }
+
+        private static Configuration ConfigureNHibernate()
+        {
+            NHibernateProfiler.Initialize();
+            var cfg = new Configuration();
+
+            cfg.DataBaseIntegration(x =>
+            {
+                x.ConnectionStringName = "default";
+                x.Driver<SqlClientDriver>();
+                x.Dialect<MsSql2008Dialect>();
+                x.IsolationLevel = IsolationLevel.RepeatableRead;
+                x.Timeout = 10; 
+                x.BatchSize = 10;
+            });
+
+            cfg.SessionFactory().GenerateStatistics();
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            return cfg;
         }
     }
 }
